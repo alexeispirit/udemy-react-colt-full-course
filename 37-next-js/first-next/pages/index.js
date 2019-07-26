e0b1339643531fe4bda69427db0d36fb6100b965
alexeispirit <alexeispirit@gmail.com>
2019-07-26 14:58:16 +0500

    295-Introducing-getInitialProps

--- 37-next-js/first-next/pages/index.js
+++ 37-next-js/first-next/pages/index.js
@@ -1,9 +1,21 @@
-const Index = () => {
-  return (
-    <div>
-      <h1>NEXT Index Page!</h1>
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static async getInitialProps() {
+    console.log("FETCHING");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>NEXT Index Page!</h1>
+      </div>
+    );
+  }
+}
 
 export default Index;
