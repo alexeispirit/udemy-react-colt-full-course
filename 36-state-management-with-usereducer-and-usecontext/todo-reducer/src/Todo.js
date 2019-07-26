1d4cddde938001c837c90fbfb9802f292e6194e6
alexeispirit <alexeispirit@gmail.com>
2019-07-26 12:15:22 +0500

    285-Optimizing-with-memo

--- 36-state-management-with-usereducer-and-usecontext/todo-reducer/src/Todo.js
+++ 36-state-management-with-usereducer-and-usecontext/todo-reducer/src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, memo } from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import CheckBox from "@material-ui/core/Checkbox";
@@ -49,4 +49,4 @@ function Todo({ id, task, completed }) {
   );
 }
 
-export default Todo;
+export default memo(Todo);
