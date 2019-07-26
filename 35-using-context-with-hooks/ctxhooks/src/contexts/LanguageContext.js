ebbd72cbc18542a009e644abf6ce54b5feab76c0
alexeispirit <alexeispirit@gmail.com>
2019-07-26 09:33:05 +0500

    274-Rewriting-context-provider-with-hooks

--- 35-using-context-with-hooks/ctxhooks/src/contexts/LanguageContext.js
+++ 35-using-context-with-hooks/ctxhooks/src/contexts/LanguageContext.js
@@ -1,24 +1,15 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, useState } from "react";
 
 export const LanguageContext = createContext();
 
-export class LanguageProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { language: "french" };
-  }
+export function LanguageProvider(props) {
+  const [language, setLanguage] = useState("french");
 
-  changeLanguage = e => {
-    this.setState({ language: e.target.value });
-  };
+  const changeLanguage = e => setLanguage(e.target.value);
 
-  render() {
-    return (
-      <LanguageContext.Provider
-        value={{ ...this.state, changeLanguage: this.changeLanguage }}
-      >
-        {this.props.children}
-      </LanguageContext.Provider>
-    );
-  }
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {props.children}
+    </LanguageContext.Provider>
+  );
 }
