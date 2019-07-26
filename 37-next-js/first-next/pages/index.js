d93102c0ce7043d30d3b359f2c738bd796ae2960
alexeispirit <alexeispirit@gmail.com>
2019-07-26 15:09:59 +0500

    296-Fetching-posts-with-getInitialProps

--- 37-next-js/first-next/pages/index.js
+++ 37-next-js/first-next/pages/index.js
@@ -1,21 +1,49 @@
-import React, { Component } from "react";
+import axios from "axios";
+// import React, { Component } from "react";
 
-class Index extends Component {
-  constructor(props) {
-    super(props);
-  }
+// class Index extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-  static async getInitialProps() {
-    console.log("FETCHING");
-  }
+//   static async getInitialProps() {
+//     const res = await axios.get("http://jsonplaceholder.typicode.com/posts");
+//     const { data } = res;
+//     return { posts: data };
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1>NEXT Index Page!</h1>
-      </div>
-    );
-  }
+//   render() {
+//     const { posts } = this.props;
+//     return (
+//       <div>
+//         <h1>NEXT Index Page!</h1>
+//         <ul>
+//           {posts.map(post => (
+//             <li key={post.id}>{post.title}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+function Index({ posts }) {
+  return (
+    <div>
+      <h1>NEXT Index Page!</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+Index.getInitialProps = async () => {
+  const res = await axios.get("http://jsonplaceholder.typicode.com/posts");
+  const { data } = res;
+  return { posts: data };
+};
 
 export default Index;
