7f8f7a5779030c544e8353a1db0370cc12f45396
alexeispirit <alexeispirit@gmail.com>
2019-07-26 15:34:50 +0500

    299-Fetching-comments

--- 37-next-js/first-next/pages/post.js
+++ 37-next-js/first-next/pages/post.js
@@ -1,11 +1,35 @@
-import { withRouter } from "next/router";
+import axios from "axios";
+// import { withRouter } from "next/router";
 
-const Post = props => {
-  return <h1>Post #{props.router.query.id}</h1>;
-};
-
-// Post.getInitialProps = async ({ query }) => {
-//   return { id: query.id };
+// const Post = props => {
+//   return <h1>Post #{props.router.query.id}</h1>;
 // };
 
-export default withRouter(Post);
+const Post = ({ id, comments }) => {
+  return (
+    <div>
+      <h1>Comments from Post #{id}</h1>
+      {comments.map(comment => (
+        <Comment {...comment} key={comment.id} />
+      ))}
+    </div>
+  );
+};
+
+const Comment = ({ email, body }) => (
+  <div>
+    <h5>{email}</h5>
+    <p>{body}</p>
+  </div>
+);
+
+Post.getInitialProps = async ({ query }) => {
+  const res = await axios.get(
+    `http://jsonplaceholder.typicode.com/comments?postId=${query.id}`
+  );
+  const { data } = res;
+  return { ...query, comments: data };
+};
+
+// export default withRouter(Post);
+export default Post;
