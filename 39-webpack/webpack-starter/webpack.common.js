6d33c3e099fa72b712e195ca09b7ec1961e30339
alexeispirit <alexeispirit@gmail.com>
2019-07-29 11:18:00 +0500

    309-Splitting-Dev-and-Production

--- 39-webpack/webpack-starter/webpack.common.js
+++ 39-webpack/webpack-starter/webpack.common.js
@@ -2,13 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   // devtool: "none",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html"
